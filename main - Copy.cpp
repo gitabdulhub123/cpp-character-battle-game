2474563346b4611b7b647b5f182d2936fa2b32d4
gitabdulhub123 <abdulrehmanvirk01@gmail.com>
2025-07-09 15:52:35 +0500

    Add files via upload

---
+++ main - Copy.cpp
@@ -0,0 +1,17 @@
+
+#include "messages.h"
+
+int main() {
+    worldbuilding(); //build the world
+    int choose=9, rounds=0;
+    choose=options();
+    while (choose!=8){ //player options with 8 being quitting the game
+        action(choose);
+        rounds++;
+        if (rounds%200==0) supply_the_markets(); //after every 200 rounds restock the markets
+        choose=options();
+    }
+    end();
+    system("pause>0");
+    return 0;
+}
